56c1e8747037d11fb36ef605b3cf11ecdfc4a9ba
Ishrat Sharmin <ishrat.cste@gmail.com>
2014-08-19 14:19:22 +0600

    delete student data added

--- Student_registraion/db.php
+++ Student_registraion/db.php
@@ -6,5 +6,29 @@ $con = mysqli_connect("localhost","root","","student_result");
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-?>
-
+//class DB {
+//    private $db_row;
+//    private $db;
+//    
+//    function __construct($db_row, $db) {
+//        $this->db_row = $db_row;
+//        $this->db = $db;
+//    }
+//    
+//    public function get_db_row() {
+//        return $this->db_row;
+//    }
+//
+//    public function get_db() {
+//        return $this->db;
+//    }
+//
+//    public function set_db_row($db_row) {
+//        $this->db_row = $db_row;
+//    }
+//
+//    public function set_db($db) {
+//        $this->db = $db;
+//    }
+//
+//}
--- Student_registraion/deleteStudent.php
+++ Student_registraion/deleteStudent.php
@@ -1,8 +1,59 @@
-<?php
+<!DOCTYPE html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Student Information</title>
+        
+         <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/sb-admin.css" rel="stylesheet">
+        <!-- Custom Fonts -->
+        <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        
+    </head>
+    <body>
+            
+      <div id="wrapper">  
+          <div id="page-wrapper" class="container"> 
+              <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Student <small>Information Details</small>
+                        </h1>
+                    </div>
+                </div>
+
+            <?php
+
+            require_once 'db.php';
+            
+            $ID = $_GET['id'];
+            
+            $query = "delete from students where id = '$ID'";
+            if(mysqli_query($con, $query))
+            {
+                echo 'Data deleted successfully';
+                echo '<a href="index.php">View Students</a>';
+            }    
+        ?>      
+              
+           </div> <!--page-wrapper ends -->       
+      </div> <!-- wrapper ends -->
+              
+    <!-- jQuery Version 1.11.0 -->
+    <script src="js/jquery-1.11.0.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="js/plugins/morris/raphael.min.js"></script>
+    <script src="js/plugins/morris/morris.min.js"></script>
+    <script src="js/plugins/morris/morris-data.js"></script>
+        
+    </body>
+</html>
 
